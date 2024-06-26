ddf08a975e8f25df2d17c382664584b897983b50
Lioneruser4 <117773578+Lioneruser4@users.noreply.github.com>
2024-06-26 19:12:21 +0300

    Update Product.js

--- backend/models/Product.js
+++ backend/models/Product.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
+
+const ProductSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    image: { type: String, required: true },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
 
