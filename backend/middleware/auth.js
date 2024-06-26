be64c43628a0ae57544031262dafce180a2fe3f4
Lioneruser4 <117773578+Lioneruser4@users.noreply.github.com>
2024-06-26 19:13:13 +0300

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -1,1 +1,14 @@
+const jwt = require('jsonwebtoken');
 
+module.exports = function (req, res, next) {
+    const token = req.header('x-auth-token');
+    if (!token) return res.status(401).send('No token, authorization denied');
+
+    try {
+        const decoded = jwt.verify(token, 'your_jwt_secret');
+        req.user = decoded.id;
+        next();
+    } catch (error) {
+        res.status(400).send('Invalid token');
+    }
+};
