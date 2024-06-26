3aedf5b4bc75f45842d22c2e376e5f36a64cae38
Lioneruser4 <117773578+Lioneruser4@users.noreply.github.com>
2024-06-26 19:14:29 +0300

    Update App.js

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,1 +1,13 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './Login';
+import Register from './Register';
+import ProductList from './ProductList';
+import ProductForm from './ProductForm';
+
+const App = () => (
+    <Router>
+        <div className="container">
+            <Switch>
+                <Route path="/" exact component={ProductList}
 
