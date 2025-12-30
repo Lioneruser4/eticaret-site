bf058829b5b6d4520c212876f9462e1a78e311b1
Lioneruser4 <117773578+Lioneruser4@users.noreply.github.com>
2025-12-30 18:07:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,52 +1,52 @@
 const express = require('express');
 const http = require('http');
-const { Server } = require('socket.io');
+const socketIo = require('socket.io');
 const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
+const io = socketIo(server);
 
-let rooms = {};
+app.use(express.static(__dirname));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+
+const rooms = {}; // { roomId: { players: {}, ball: {}, timeLeft, scores: {blue:0, red:0} } }
 
 io.on('connection', (socket) => {
-    socket.on('createRoom', (data) => {
-        const roomId = "Room_" + Math.random().toString(36).substr(2, 9);
-        rooms[roomId] = {
-            id: roomId,
-            duration: data.duration * 60,
-            timer: data.duration * 60,
-            players: {},
-            ball: { x: 0, y: 1, z: 0, vx: 0, vz: 0 },
-            score: { red: 0, blue: 0 }
-        };
+    console.log('Bağlandı:', socket.id);
+
+    socket.on('joinLobby', (data) => {
+        socket.userData = data;
+    });
+
+    socket.on('createRoom', ({ time }) => {
+        const roomId = Math.random().toString(36).substr(2, 8).toUpperCase();
+        rooms[roomId] = { players: {}, scores: { blue: 0, red: 0 }, timeLeft: time };
         socket.emit('roomCreated', roomId);
+        socket.emit('joinRoomSuccess', { roomId });
     });
 
-    socket.on('joinRoom', ({ roomId, user }) => {
-        if (!rooms[roomId]) return;
-        socket.join(roomId);
-        rooms[roomId].players[socket.id] = {
-            id: socket.id,
-            name: user.name,
-            img: user.img,
-            team: 'red', // Varsayılan
-            x: Math.random() * 10,
-            z: Math.random() * 10,
-            anim: 'idle'
-        };
-        io.to(roomId).emit('updateState', rooms[roomId]);
-    });
-
-    socket.on('move', (data) => {
-        if (rooms[data.roomId] && rooms[data.roomId].players[socket.id]) {
-            let p = rooms[data.roomId].players[socket.id];
-            p.x = data.x;
-            p.z = data.z;
-            p.anim = data.anim;
-            socket.to(data.roomId).emit('playerMoved', { id: socket.id, x: p.x, z: p.z, anim: p.anim });
+    socket.on('joinRoom', (roomId) => {
+        if (rooms[roomId]) {
+            socket.join(roomId);
+            const team = Object.values(rooms[roomId].players).filter(p => p.team === 'blue').length <= 
+                        Object.values(rooms[roomId].players).filter(p => p.team === 'red').length ? 'blue' : 'red';
+            rooms[roomId].players[socket.id] = { team, username: socket.userData.username };
+            io.to(roomId).emit('playerJoined', rooms[roomId].players);
+            socket.emit('joinRoomSuccess', { roomId, team });
         }
+    });
+
+    socket.on('kick', ({ power }) => {
+        const roomId = [...socket.rooms][1];
+        if (roomId) io.to(roomId).emit('kickBall', power);
+    });
+
+    socket.on('disconnect', () => {
+        // Oda temizleme vs.
     });
 });
 
-server.listen(10000, () => console.log('Saskio Arena Running...'));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Sunucu ${PORT}'da çalışıyor`));
